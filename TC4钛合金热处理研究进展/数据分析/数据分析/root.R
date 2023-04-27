5404d968d0cc2eef277df3f45c48ac098fe30001
aornus <me@si-on.top>
2023-04-27 19:48:10 +0800

    查重 31%

--- TC4钛合金热处理研究进展/数据分析/数据分析/root.R
+++ TC4钛合金热处理研究进展/数据分析/数据分析/root.R
@@ -1,10 +1,14 @@
-pmar <- par(mar = c(5.1, 4.1, 4.1, 6.1)) #改版画布版式大小
-with(iris, scatter3D(x = Sepal.Length, y = Sepal.Width, z = Petal.Length,
-                     pch = 21, cex = 1.5,col="black",bg="#F57446",
-                     xlab = "x",
-                     ylab = "y",
-                     zlab = "zb", 
-                     ticktype = "detailed",bty = "f",box = TRUE,
-                     theta = 60, phi = 20, d=3,
-                     colkey = FALSE)
+library(echarts4r)
+
+df <- data.frame(
+  x = seq(50),
+  y = rnorm(50, 10, 3),
+  z = rnorm(50, 11, 2),
+  w = rnorm(50, 9, 2)
 )
+
+df |> 
+  e_charts(x) |> 
+  e_line(z) |> 
+  e_area(w) |> 
+  e_title("Line and area charts")
